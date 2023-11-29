27bfa988219acc9964c5a23a6a6eafac063c1aa2
Сергей Баданов <spy02m@gmail.com>
2023-11-29 15:05:34 +0400

    Added dogstring

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from random import randint
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Функция навыка attack."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -11,20 +12,22 @@ def attack(char_name: str, char_class: str) -> str:
     if char_class == 'healer':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(-3, -1)}')
-    return 'Неверный ввод'
+    return (f'{char_name} не нанес урон')
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Функция навыка defence."""
     if char_class == 'warrior':
         return f'{char_name} блокировал {10 + randint(5, 10)} урона'
     if char_class == 'mage':
         return f'{char_name} блокировал {10 + randint(-2, 2)} урона'
     if char_class == 'healer':
         return f'{char_name} блокировал {10 + randint(2, 5)} урона'
-    return 'Неверный ввод'
+    return (f'{char_name} не блокировал урон')
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Функция навыка special."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение «Выносливость '
                 f'{80 + 25}»')
@@ -32,10 +35,11 @@ def special(char_name: str, char_class: str) -> str:
         return (f'{char_name} применил специальное умение «Атака {5 + 40}»')
     if char_class == 'healer':
         return (f'{char_name} применил специальное умение «Защита {10 + 30}»')
-    return 'Неверный ввод'
+    return (f'{char_name} не применил специальное умение')
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Функция старта тренировки."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -60,6 +64,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Функция выбора персонажа."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -82,6 +87,7 @@ def choice_char_class() -> str:
 
 
 def main() -> None:
+    """Приветственная функция."""
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
